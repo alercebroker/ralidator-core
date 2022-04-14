07b59e7c55e3ba21b9ed22842dec97012d678584
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-04-14 14:00:17 -0400

    fix: imports + black formatter

--- ralidator_core/tests/test_ralidator_core/test_ralidator_core.py
+++ ralidator_core/tests/test_ralidator_core/test_ralidator_core.py
@@ -1,4 +1,4 @@
-from package.ralidator_core.ralidator_core import Ralidator
+from ralidator_core.ralidator_core import Ralidator
 from datetime import datetime, timezone, timedelta
 import jwt
 
@@ -8,84 +8,84 @@ TEST_SECRET_KEY = "secret_key"
 
 
 def test_ralidator_constructor():
-    test_callbacks_dict = {
-        "filter1": lambda x: x,
-        "filter2": lambda x: x
-    }
+    test_callbacks_dict = {"filter1": lambda x: x, "filter2": lambda x: x}
 
     # exception?
     ralidator = Ralidator(test_callbacks_dict)
+
 
 def test_authenticate_token_valid():
     test_callbacks_dict = {
         "filter1": lambda x: x,
         "filter2": lambda x: x,
-        "filter3": lambda x: x
+        "filter3": lambda x: x,
     }
     token = {
-        'token_type': 'access',
-        'exp': datetime.now(tz=timezone.utc) + timedelta(hours=1),
-        'jti': 'test_jti',
-        'user_id': 1,
+        "token_type": "access",
+        "exp": datetime.now(tz=timezone.utc) + timedelta(hours=1),
+        "jti": "test_jti",
+        "user_id": 1,
         "permisions": ["permision1", "permision2"],
-        "filters": ["filter1", "filter2"    ]
+        "filters": ["filter1", "filter2"],
     }
     encripted_token = jwt.encode(token, TEST_SECRET_KEY, algorithm="HS256")
 
     ralidator = Ralidator(test_callbacks_dict)
     ralidator.authenticate_token(TEST_SECRET_KEY, encripted_token)
-    
+
     assert ralidator.given_permisions == ["permision1", "permision2"]
     assert ralidator.given_filters == ["filter1", "filter2"]
+
 
 def test_authenticate_token_invalid():
     test_callbacks_dict = {
         "filter1": lambda x: x,
         "filter2": lambda x: x,
-        "filter3": lambda x: x
+        "filter3": lambda x: x,
     }
     # token expired
     token = {
-        'token_type': 'access',
-        'exp': datetime.now(tz=timezone.utc) + timedelta(hours=-1),
-        'jti': 'test_jti',
-        'user_id': 1,
+        "token_type": "access",
+        "exp": datetime.now(tz=timezone.utc) + timedelta(hours=-1),
+        "jti": "test_jti",
+        "user_id": 1,
         "permisions": ["permision1", "permision2"],
-        "filters": ["filter1", "filter2"]
+        "filters": ["filter1", "filter2"],
     }
     encripted_token = jwt.encode(token, TEST_SECRET_KEY, algorithm="HS256")
-
 
     ralidator = Ralidator(test_callbacks_dict)
     # raise error? return result? set variable to be readed in the future, in check allowed?
     ralidator.authenticate_token(TEST_SECRET_KEY, encripted_token)
 
+
 def test_authenticate_token_default_token():
     test_callbacks_dict = {
         "filter1": lambda x: x,
         "filter2": lambda x: x,
-        "filter3": lambda x: x
+        "filter3": lambda x: x,
     }
     ralidator = Ralidator(test_callbacks_dict)
     ralidator.authenticate_token(TEST_SECRET_KEY, None)
-    
+
     # definir donde y cuales
     assert ralidator.given_permisions == []
     assert ralidator.given_filters == []
+
 
 def test_check_allowed_allowed():
     test_callbacks_dict = {
         "filter1": lambda x: x,
         "filter2": lambda x: x,
-        "filter3": lambda x: x
+        "filter3": lambda x: x,
     }
     token = {
-        'token_type': 'access',
-        'exp': datetime.now(tz=timezone.utc) + timedelta(hours=1),
-        'jti': 'test_jti',
-        'user_id': 1,
+        "token_type": "access",
+        "exp": datetime.now(tz=timezone.utc) + timedelta(hours=1),
+        "jti": "test_jti",
+        "user_id": 1,
         "permisions": ["permision1", "permision2"],
-        "filters": ["filter1", "filter2"    ]
+        "filters": ["filter1", "filter2"],
     }
     encripted_token = jwt.encode(token, TEST_SECRET_KEY, algorithm="HS256")
 
@@ -95,19 +95,20 @@ def test_check_allowed_allowed():
     result = ralidator.check_if_allowed()
     assert result
 
+
 def test_check_allowed_not_allowed():
     test_callbacks_dict = {
         "filter1": lambda x: x,
         "filter2": lambda x: x,
-        "filter3": lambda x: x
+        "filter3": lambda x: x,
     }
     token = {
-        'token_type': 'access',
-        'exp': datetime.now(tz=timezone.utc) + timedelta(hours=1),
-        'jti': 'test_jti',
-        'user_id': 1,
+        "token_type": "access",
+        "exp": datetime.now(tz=timezone.utc) + timedelta(hours=1),
+        "jti": "test_jti",
+        "user_id": 1,
         "permisions": ["permision1", "permision2"],
-        "filters": ["filter1", "filter2"    ]
+        "filters": ["filter1", "filter2"],
     }
     encripted_token = jwt.encode(token, TEST_SECRET_KEY, algorithm="HS256")
 
@@ -116,6 +117,7 @@ def test_check_allowed_not_allowed():
     ralidator.set_required_permisions(["permision3"])
     result = ralidator.check_if_allowed()
     assert not result
+
 
 def test_apply_filters():
     # pendiente, una idea es aplicar todos los filtros a un
--- ralidator_core/tests/test_utils/test_decript_and_parse.py
+++ ralidator_core/tests/test_utils/test_decript_and_parse.py
@@ -1,6 +1,6 @@
 import jwt
 from datetime import datetime, timedelta, timezone
-from package.utils.utils import decript_and_parse
+from ralidator_core.utils.utils import decript_and_parse
 
 
 TEST_SECRET_KEY = "secret_key"
@@ -8,16 +8,17 @@ TEST_SECRET_KEY = "secret_key"
 
 def generate_valid_token(remove_keys=[]):
     token = {
-        'token_type': 'access',
-        'exp': datetime.now(tz=timezone.utc) + timedelta(hours=1),
-        'jti': 'test_jti',
-        'user_id': 1,
+        "token_type": "access",
+        "exp": datetime.now(tz=timezone.utc) + timedelta(hours=1),
+        "jti": "test_jti",
+        "user_id": 1,
         "permisions": ["permision1", "permision2"],
-        "filters": ["filter1", "filter2"]
+        "filters": ["filter1", "filter2"],
     }
     for key in remove_keys:
         token.pop(key)
     return token
+
 
 def test_decript_correct_token():
     test_token = generate_valid_token()
@@ -36,11 +37,13 @@ def test_decript_correct_token():
     assert result[0]
     assert result[1] == test_token
 
+
 def test_decript_bad_token():
     encripted_test_token = "bad string to test"
     result = decript_and_parse(encripted_test_token, TEST_SECRET_KEY)
 
     assert not result[0]
+
 
 def test_decipt_invalid_token():
     # expired
@@ -87,4 +90,3 @@ def test_decipt_invalid_token():
     result = decript_and_parse(encripted_test_token, TEST_SECRET_KEY)
 
     assert not result[0]
-
